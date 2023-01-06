4d8f115614ea4bd0c22585848238be36866ad0e5
Hamzagouissem <gouissemhamza7@gmail.com>
2023-01-06 22:11:17 +0100

    routproject

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './App.css';
+import AddNewMovie from './Components/AddNewMovie';
+import MovieList from './Components/MovieList';
+import SearchBar from './Components/SearchBar';
+import { moviesData } from './Data';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Description from './Components/Description';
+
+
+function App() {
+  const [movies, setMovies] = useState(moviesData);
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const handleChange = (b) => {
+    setText(b);
+  }
+
+  const handleRating = (s) => {
+    setRating();
+  }
+
+  //to delete a movie
+  const handleDelete = (id) => {
+    setMovies(movies.filter(movie => movie.id !== id));
+  }
+
+  //to add a new movie
+  const handleAdd = (newMovie) => {
+    setMovies([...movies, newMovie]);
+
+  }
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<div>
+            <SearchBar text={text} rating={rating} handleChange={handleChange} handleRating={handleRating} />
+            <MovieList data={movies.filter(elem => elem.name.toLocaleLowerCase().includes(text.toLocaleLowerCase()) && elem.rating >= rating)} deleteMovie={handleDelete} />
+            <AddNewMovie handleAdd={handleAdd} />
+          </div>} />
+          <Route path='/description/:id' element={<Description movies={movies} />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export const moviesData=[
+    {
+        id:Math.random(),
+        image:"https://dx35vtwkllhj9.cloudfront.net/pathelive/ima/images/regions/fr/onesheet.jpg",
+        rating:5,
+        name:"Imma",
+    
+    },
+    {
+        id:Math.random(),
+        image:"https://c8.alamy.com/compfr/2f80y7e/une-affiche-de-film-d-action-b-movie-vintage-pour-l-ark-of-le-dieu-soleil-2f80y7e.jpg",
+        rating:4,
+        name:"The Arc Of The Sun God",
+        
+    },
+    {
+        id:Math.random(),
+        image:"https://c8.alamy.com/compfr/2f80y82/une-affiche-de-film-d-action-b-movie-vintage-nom-du-code-zebra-2f80y82.jpg",
+        rating:3,
+        name:"ZEBRA",
+    },
+    {
+        id:Math.random(),
+        image:"https://ae01.alicdn.com/kf/HTB1omVZIpXXXXcrXVXXq6xXFXXX6.jpg",
+        rating:4,
+        name:"Blacklight",
+    
+    },
+   
+]
